8d9d1e77381c28273fbaffc9dbfb5890fdee0ec6
姚嘉松 <yaojiasong@gooagoo.com>
2017-11-01 17:37:49 +0800

    组件转移 无线 UI 组件 refresh-control

---
+++ src/RefreshControl.jsx
@@ -0,0 +1,25 @@
+/* tslint:disable:no-unused-variable */
+import '../style';
+import React from 'react';
+/* tslint:enable:no-unused-variable */
+import ListView from 'rmc-list-view';
+import Icon from '@gag/icon';
+// import tsPropsType from './PropsType';
+import assign from 'object-assign';
+
+ListView.RefreshControl.defaultProps = assign({}, ListView.RefreshControl.defaultProps, {
+  prefixCls: 'am-refresh-control',
+  icon: [
+    <div key="0" className="am-refresh-control-pull">
+      <span>下拉可以刷新</span>
+    </div>,
+    <div key="1" className="am-refresh-control-release">
+      <span>松开立即刷新</span>
+    </div>,
+  ],
+  loading: <Icon type="loading" />,
+  refreshing: false,
+  distanceToRefresh: 50 / 2 * (window.devicePixelRatio || 2),
+});
+
+export default ListView.RefreshControl;
